88f0c57f538d6b71bda138acc76ffd09aa7b1b18
InfinityLoop <swsoyee@gmail.com>
2021-02-14 22:59:47 +0900

    feat: implement add function

---
+++ R/add.R
@@ -0,0 +1,14 @@
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
