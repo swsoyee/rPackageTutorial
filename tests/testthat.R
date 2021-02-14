2655b2a9b936148a33ae50c30d7e8c6bb7ea7095
InfinityLoop <swsoyee@gmail.com>
2021-02-15 00:23:21 +0900

    test: use usethis::use_testthat() to setup test and add test for add()

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rPackageTutorial)
+
+test_check("rPackageTutorial")
---
+++ tests/testthat/test-add.R
@@ -0,0 +1,3 @@
+test_that("add() function return the sum of two number", {
+  expect_equal(add(1, 2), 3)
+})
